de1542da93d3f0b7e8642bcc9bcb06f920ebb4a0
nasywanfarid <nasywanfarid88@gmail.com>
2020-11-27 22:55:34 +0700

    crud

---
+++ pesan.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Warung Kebab</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"><script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div>
+		<div class="loader"></div>
+	</div>
+		
+	</div>
+	<header>
+		<div class="container">
+			<h1><a href="index.html">WARUNG KEBAB</a></h1>
+			<ul>
+				<li><a href="index.html">HOME</a></li>
+				<li><a href="about.html">FOOD</a></li>
+				<li><a href="service.html">DRINK</a></li>
+				<li><a href="contact.html">CONTACT</a></li>
+				<li class="active"><a href="pesan.php">TAMBAH MENU</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<section class="label">
+		<div class="container">
+			<p>Home / Tambah Menu</p>
+		</div>
+	</section>
+
+	<section class="service">
+		<div class="container">
+			<button><a href="index.php">Tambah Menu</a></button>
+		    <br/><br/>
+
+		    <form action="add.php" method="post" name="form1">
+		        <table width="25%" border="0">
+    			</table>
+			</form>
+		</div>
+	</section>
+</body>
+</html>
